c95df0e068bc7b3c1bd52d8aad52a6d16d85f60a
Akweley Okai <akweleyokaiofficial@gmail.com>
2024-02-12 00:44:03 +0000

    second commit

--- main/models.py
+++ main/models.py
@@ -27,3 +27,29 @@ class ClassAttendance(models.Model):
     organizer =models.CharField(max_length=50)
     cohort = models.ForeignKey(Cohort, on_delete=models.CASCADE)
     venue = models.TextField()
+
+
+class Query(models.Model):
+    RESOLUTION_STATUS_CHOICES = [
+        ('PENDING', 'Pending'),
+        ('IN_PROGRESS', 'In Progress'),
+        ('DECLINED', 'Declined'),
+        ('RESOLVED', 'Resolved')
+    ]
+
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    submitted_by = models.ForeignKey(IMUser, on_delete=models.CASCADE, related_name='query_submitter')
+    assigned_to = models.ForeignKey(IMUser, on_delete=models.CASCADE, related_name='query_assignee')
+    resolution_status = models.CharField(max_length=50, choices=RESOLUTION_STATUS_CHOICES)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(IMUser, on_delete=models.CASCADE, related_name='query_author')
+
+
+class QueryComment(models.Model):
+    query = models.ForeignKey(Query, on_delete=models.CASCADE)
+    comment = models.TextField()
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(IMUser, on_delete=models.CASCADE, related_name='query_comment_author')
